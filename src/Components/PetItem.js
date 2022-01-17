69627198be34ca8e559e956de4f7ea81fc859977
alsharhanaisha <alsharhanaisha@gmail.com>
2022-01-17 20:34:19 +0300

    Done

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,12 +1,17 @@
-export default function PetItem(props) {
-  const pet = props.pet;
+import React, { useState } from "react";
+
+export default function PetItem({ pet }) {
+  const [petImg, setpetImg] = useState(pet.image);
+  const changePetImage = () => {
+    setpetImg(pet.image2);
+  };
   return (
     <div class="col-lg-4 col-md-8 col-sm-10">
       <div class="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={petImg} />
         <div class="content">
           <h3>{pet.name}</h3>
-          <button type="button" class="btn btn-info">
+          <button type="button" class="btn btn-info" onClick={changePetImage}>
             Pet
           </button>
         </div>
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,7 +1,24 @@
-import PetItem from './PetItem';
+import { useState } from "react";
+import PetItem from "./PetItem";
 
 export default function PetsList(props) {
-  const pets = props.pets.map((pet) => <PetItem key={pet.id} pet={pet} />);
+  const [query, setQuery] = useState("");
+  const changeQuery = (event) => {
+    setQuery(event.target.value);
+  };
+  const [type, setType] = useState("");
+  const selectPet = (event) => {
+    setType(event.target.value);
+  };
+  const pets = props.pets
+    .filter(
+      (pet) =>
+        pet.name.toLowerCase().includes(query.toLowerCase()) &&
+        pet.type.includes(type)
+    )
+    .map((pet) => <PetItem key={pet.id} pet={pet} type={pet.type} />);
+  // && props.pets.filter((pet) => pet.type === type) =>
+
   return (
     <section id="doctors" class="doctor-section pt-140">
       <div class="container">
@@ -18,11 +35,12 @@ export default function PetsList(props) {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={changeQuery}
                 />
               </div>
               <br />
               Type:
-              <select class="form-select">
+              <select class="form-select" onChange={selectPet}>
                 <option value="" selected>
                   All
                 </option>
